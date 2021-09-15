88e050e46d37b0d4d8346a3fc8906975d10da3ab
SulimanDahan <90277725+SulimanDahan@users.noreply.github.com>
2021-09-15 02:30:11 -0700

    Add files via upload

---
+++ School_HR_API/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_HR_API
+{
+    public interface IEmployee
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        string Job { get; set; }
+
+        decimal Salary { get; set; }
+    }
+}
