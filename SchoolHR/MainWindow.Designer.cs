e7b629f4ae7dfba72c84196ebf0dfe034e0e36be
SulimanDahan <90277725+SulimanDahan@users.noreply.github.com>
2021-09-15 02:35:20 -0700

    Add files via upload

---
+++ SchoolHR/MainWindow.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace SchoolHR
+{
+    partial class AddEmpWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputContainer = new System.Windows.Forms.GroupBox();
+            this.LastProfileB = new System.Windows.Forms.Button();
+            this.NextProfileB = new System.Windows.Forms.Button();
+            this.PreviousProfileB = new System.Windows.Forms.Button();
+            this.FirstProfileB = new System.Windows.Forms.Button();
+            this.ClearEmpBtn = new System.Windows.Forms.Button();
+            this.AddEmpBtn = new System.Windows.Forms.Button();
+            this.EmpJobCombo = new System.Windows.Forms.ComboBox();
+            this.EmpSalaryTxt = new System.Windows.Forms.TextBox();
+            this.EmpNameTxt = new System.Windows.Forms.TextBox();
+            this.EmpIDTxt = new System.Windows.Forms.TextBox();
+            this.EmpSalaryLbl = new System.Windows.Forms.Label();
+            this.EmpTypeLbl = new System.Windows.Forms.Label();
+            this.EmpNameLbl = new System.Windows.Forms.Label();
+            this.EmpIDLbl = new System.Windows.Forms.Label();
+            this.DataContainer = new System.Windows.Forms.Panel();
+            this.NumOfEmpLbl = new System.Windows.Forms.Label();
+            this.TotalSalShowLbl = new System.Windows.Forms.Label();
+            this.TotalSaleriesLbl = new System.Windows.Forms.Label();
+            this.CountEmpLbl = new System.Windows.Forms.Label();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.HeadContainer = new System.Windows.Forms.Panel();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.InputContainer.SuspendLayout();
+            this.DataContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.HeadContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputContainer
+            // 
+            this.InputContainer.Controls.Add(this.LastProfileB);
+            this.InputContainer.Controls.Add(this.NextProfileB);
+            this.InputContainer.Controls.Add(this.PreviousProfileB);
+            this.InputContainer.Controls.Add(this.FirstProfileB);
+            this.InputContainer.Controls.Add(this.ClearEmpBtn);
+            this.InputContainer.Controls.Add(this.AddEmpBtn);
+            this.InputContainer.Controls.Add(this.EmpJobCombo);
+            this.InputContainer.Controls.Add(this.EmpSalaryTxt);
+            this.InputContainer.Controls.Add(this.EmpNameTxt);
+            this.InputContainer.Controls.Add(this.EmpIDTxt);
+            this.InputContainer.Controls.Add(this.EmpSalaryLbl);
+            this.InputContainer.Controls.Add(this.EmpTypeLbl);
+            this.InputContainer.Controls.Add(this.EmpNameLbl);
+            this.InputContainer.Controls.Add(this.EmpIDLbl);
+            this.InputContainer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.InputContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputContainer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InputContainer.Location = new System.Drawing.Point(0, 93);
+            this.InputContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.InputContainer.Name = "InputContainer";
+            this.InputContainer.Padding = new System.Windows.Forms.Padding(4);
+            this.InputContainer.Size = new System.Drawing.Size(700, 335);
+            this.InputContainer.TabIndex = 1;
+            this.InputContainer.TabStop = false;
+            this.InputContainer.Text = "Employee Info";
+            // 
+            // LastProfileB
+            // 
+            this.LastProfileB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LastProfileB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LastProfileB.Location = new System.Drawing.Point(533, 203);
+            this.LastProfileB.Name = "LastProfileB";
+            this.LastProfileB.Size = new System.Drawing.Size(127, 27);
+            this.LastProfileB.TabIndex = 14;
+            this.LastProfileB.Text = "Last";
+            this.LastProfileB.UseVisualStyleBackColor = true;
+            this.LastProfileB.Click += new System.EventHandler(this.LastProfileB_Click);
+            // 
+            // NextProfileB
+            // 
+            this.NextProfileB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NextProfileB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NextProfileB.Location = new System.Drawing.Point(369, 203);
+            this.NextProfileB.Name = "NextProfileB";
+            this.NextProfileB.Size = new System.Drawing.Size(127, 27);
+            this.NextProfileB.TabIndex = 13;
+            this.NextProfileB.Text = "Next";
+            this.NextProfileB.UseVisualStyleBackColor = true;
+            this.NextProfileB.Click += new System.EventHandler(this.NextProfileB_Click);
+            // 
+            // PreviousProfileB
+            // 
+            this.PreviousProfileB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PreviousProfileB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PreviousProfileB.Location = new System.Drawing.Point(208, 203);
+            this.PreviousProfileB.Name = "PreviousProfileB";
+            this.PreviousProfileB.Size = new System.Drawing.Size(127, 27);
+            this.PreviousProfileB.TabIndex = 12;
+            this.PreviousProfileB.Text = "Previous";
+            this.PreviousProfileB.UseVisualStyleBackColor = true;
+            this.PreviousProfileB.Click += new System.EventHandler(this.PreviousProfileB_Click);
+            // 
+            // FirstProfileB
+            // 
+            this.FirstProfileB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FirstProfileB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FirstProfileB.Location = new System.Drawing.Point(46, 203);
+            this.FirstProfileB.Name = "FirstProfileB";
+            this.FirstProfileB.Size = new System.Drawing.Size(127, 27);
+            this.FirstProfileB.TabIndex = 11;
+            this.FirstProfileB.Text = "First";
+            this.FirstProfileB.UseVisualStyleBackColor = true;
+            this.FirstProfileB.Click += new System.EventHandler(this.FirstProfileB_Click);
+            // 
+            // ClearEmpBtn
+            // 
+            this.ClearEmpBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClearEmpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearEmpBtn.Location = new System.Drawing.Point(533, 115);
+            this.ClearEmpBtn.Name = "ClearEmpBtn";
+            this.ClearEmpBtn.Size = new System.Drawing.Size(127, 27);
+            this.ClearEmpBtn.TabIndex = 10;
+            this.ClearEmpBtn.Text = "Clear";
+            this.ClearEmpBtn.UseVisualStyleBackColor = true;
+            this.ClearEmpBtn.Click += new System.EventHandler(this.ClearEmpBtn_Click);
+            // 
+            // AddEmpBtn
+            // 
+            this.AddEmpBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddEmpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddEmpBtn.Location = new System.Drawing.Point(533, 80);
+            this.AddEmpBtn.Name = "AddEmpBtn";
+            this.AddEmpBtn.Size = new System.Drawing.Size(127, 27);
+            this.AddEmpBtn.TabIndex = 9;
+            this.AddEmpBtn.Text = "Add";
+            this.AddEmpBtn.UseVisualStyleBackColor = true;
+            this.AddEmpBtn.Click += new System.EventHandler(this.AddEmpBtn_Click);
+            // 
+            // EmpJobCombo
+            // 
+            this.EmpJobCombo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EmpJobCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EmpJobCombo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.EmpJobCombo.FormattingEnabled = true;
+            this.EmpJobCombo.Location = new System.Drawing.Point(178, 116);
+            this.EmpJobCombo.Name = "EmpJobCombo";
+            this.EmpJobCombo.Size = new System.Drawing.Size(309, 27);
+            this.EmpJobCombo.TabIndex = 7;
+            // 
+            // EmpSalaryTxt
+            // 
+            this.EmpSalaryTxt.Location = new System.Drawing.Point(178, 154);
+            this.EmpSalaryTxt.Name = "EmpSalaryTxt";
+            this.EmpSalaryTxt.Size = new System.Drawing.Size(309, 27);
+            this.EmpSalaryTxt.TabIndex = 8;
+            this.EmpSalaryTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EmpSalaryTxt_KeyPress);
+            // 
+            // EmpNameTxt
+            // 
+            this.EmpNameTxt.Location = new System.Drawing.Point(178, 81);
+            this.EmpNameTxt.Name = "EmpNameTxt";
+            this.EmpNameTxt.Size = new System.Drawing.Size(309, 27);
+            this.EmpNameTxt.TabIndex = 5;
+            // 
+            // EmpIDTxt
+            // 
+            this.EmpIDTxt.Location = new System.Drawing.Point(178, 43);
+            this.EmpIDTxt.Name = "EmpIDTxt";
+            this.EmpIDTxt.Size = new System.Drawing.Size(309, 27);
+            this.EmpIDTxt.TabIndex = 4;
+            this.EmpIDTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EmpIDTxt_KeyPress);
+            // 
+            // EmpSalaryLbl
+            // 
+            this.EmpSalaryLbl.AutoSize = true;
+            this.EmpSalaryLbl.Location = new System.Drawing.Point(42, 157);
+            this.EmpSalaryLbl.Name = "EmpSalaryLbl";
+            this.EmpSalaryLbl.Size = new System.Drawing.Size(126, 19);
+            this.EmpSalaryLbl.TabIndex = 3;
+            this.EmpSalaryLbl.Text = "Employee Salary";
+            // 
+            // EmpTypeLbl
+            // 
+            this.EmpTypeLbl.AutoSize = true;
+            this.EmpTypeLbl.Location = new System.Drawing.Point(42, 119);
+            this.EmpTypeLbl.Name = "EmpTypeLbl";
+            this.EmpTypeLbl.Size = new System.Drawing.Size(113, 19);
+            this.EmpTypeLbl.TabIndex = 2;
+            this.EmpTypeLbl.Text = "Employee type";
+            // 
+            // EmpNameLbl
+            // 
+            this.EmpNameLbl.AutoSize = true;
+            this.EmpNameLbl.Location = new System.Drawing.Point(42, 84);
+            this.EmpNameLbl.Name = "EmpNameLbl";
+            this.EmpNameLbl.Size = new System.Drawing.Size(124, 19);
+            this.EmpNameLbl.TabIndex = 1;
+            this.EmpNameLbl.Text = "Employee Name";
+            // 
+            // EmpIDLbl
+            // 
+            this.EmpIDLbl.AutoSize = true;
+            this.EmpIDLbl.Location = new System.Drawing.Point(42, 46);
+            this.EmpIDLbl.Name = "EmpIDLbl";
+            this.EmpIDLbl.Size = new System.Drawing.Size(100, 19);
+            this.EmpIDLbl.TabIndex = 0;
+            this.EmpIDLbl.Text = "Employee ID";
+            // 
+            // DataContainer
+            // 
+            this.DataContainer.Controls.Add(this.NumOfEmpLbl);
+            this.DataContainer.Controls.Add(this.TotalSalShowLbl);
+            this.DataContainer.Controls.Add(this.TotalSaleriesLbl);
+            this.DataContainer.Controls.Add(this.CountEmpLbl);
+            this.DataContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.DataContainer.Location = new System.Drawing.Point(0, 329);
+            this.DataContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.DataContainer.Name = "DataContainer";
+            this.DataContainer.Size = new System.Drawing.Size(700, 99);
+            this.DataContainer.TabIndex = 2;
+            // 
+            // NumOfEmpLbl
+            // 
+            this.NumOfEmpLbl.AutoSize = true;
+            this.NumOfEmpLbl.Location = new System.Drawing.Point(224, 19);
+            this.NumOfEmpLbl.Name = "NumOfEmpLbl";
+            this.NumOfEmpLbl.Size = new System.Drawing.Size(18, 19);
+            this.NumOfEmpLbl.TabIndex = 7;
+            this.NumOfEmpLbl.Text = "0";
+            // 
+            // TotalSalShowLbl
+            // 
+            this.TotalSalShowLbl.AutoSize = true;
+            this.TotalSalShowLbl.Location = new System.Drawing.Point(224, 58);
+            this.TotalSalShowLbl.Name = "TotalSalShowLbl";
+            this.TotalSalShowLbl.Size = new System.Drawing.Size(18, 19);
+            this.TotalSalShowLbl.TabIndex = 6;
+            this.TotalSalShowLbl.Text = "0";
+            // 
+            // TotalSaleriesLbl
+            // 
+            this.TotalSaleriesLbl.AutoSize = true;
+            this.TotalSaleriesLbl.Location = new System.Drawing.Point(39, 58);
+            this.TotalSaleriesLbl.Name = "TotalSaleriesLbl";
+            this.TotalSaleriesLbl.Size = new System.Drawing.Size(115, 19);
+            this.TotalSaleriesLbl.TabIndex = 5;
+            this.TotalSaleriesLbl.Text = "Total Saleries :";
+            // 
+            // CountEmpLbl
+            // 
+            this.CountEmpLbl.AutoSize = true;
+            this.CountEmpLbl.Location = new System.Drawing.Point(39, 19);
+            this.CountEmpLbl.Name = "CountEmpLbl";
+            this.CountEmpLbl.Size = new System.Drawing.Size(136, 19);
+            this.CountEmpLbl.TabIndex = 4;
+            this.CountEmpLbl.Text = "Employee Count :";
+            // 
+            // Logo
+            // 
+            this.Logo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Logo.Image = global::SchoolHR.Properties.Resources.AddIcon;
+            this.Logo.Location = new System.Drawing.Point(0, 0);
+            this.Logo.Margin = new System.Windows.Forms.Padding(4);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(98, 93);
+            this.Logo.TabIndex = 1;
+            this.Logo.TabStop = false;
+            // 
+            // HeadContainer
+            // 
+            this.HeadContainer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.HeadContainer.Controls.Add(this.TitleLbl);
+            this.HeadContainer.Controls.Add(this.Logo);
+            this.HeadContainer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeadContainer.Location = new System.Drawing.Point(0, 0);
+            this.HeadContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.HeadContainer.Name = "HeadContainer";
+            this.HeadContainer.Size = new System.Drawing.Size(700, 93);
+            this.HeadContainer.TabIndex = 0;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TitleLbl.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
+            this.TitleLbl.Location = new System.Drawing.Point(119, 32);
+            this.TitleLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(207, 33);
+            this.TitleLbl.TabIndex = 2;
+            this.TitleLbl.Text = "Add employee";
+            // 
+            // AddEmpWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 428);
+            this.Controls.Add(this.DataContainer);
+            this.Controls.Add(this.InputContainer);
+            this.Controls.Add(this.HeadContainer);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddEmpWin";
+            this.Text = "Add";
+            this.Load += new System.EventHandler(this.AddEmpWin_Load);
+            this.InputContainer.ResumeLayout(false);
+            this.InputContainer.PerformLayout();
+            this.DataContainer.ResumeLayout(false);
+            this.DataContainer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.HeadContainer.ResumeLayout(false);
+            this.HeadContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox InputContainer;
+        private System.Windows.Forms.Panel DataContainer;
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.Panel HeadContainer;
+        private System.Windows.Forms.Label TitleLbl;
+        private System.Windows.Forms.TextBox EmpSalaryTxt;
+        private System.Windows.Forms.TextBox EmpNameTxt;
+        private System.Windows.Forms.TextBox EmpIDTxt;
+        private System.Windows.Forms.Label EmpSalaryLbl;
+        private System.Windows.Forms.Label EmpTypeLbl;
+        private System.Windows.Forms.Label EmpNameLbl;
+        private System.Windows.Forms.Label EmpIDLbl;
+        private System.Windows.Forms.Button ClearEmpBtn;
+        private System.Windows.Forms.Button AddEmpBtn;
+        private System.Windows.Forms.ComboBox EmpJobCombo;
+        private System.Windows.Forms.Label NumOfEmpLbl;
+        private System.Windows.Forms.Label TotalSalShowLbl;
+        private System.Windows.Forms.Label TotalSaleriesLbl;
+        private System.Windows.Forms.Label CountEmpLbl;
+        private System.Windows.Forms.Button LastProfileB;
+        private System.Windows.Forms.Button NextProfileB;
+        private System.Windows.Forms.Button PreviousProfileB;
+        private System.Windows.Forms.Button FirstProfileB;
+    }
+}
---
+++ SchoolHR/MainWindow.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using School_HR_API;
+
+namespace SchoolHR
+{
+    public enum EmployeeType
+    {
+        Teacher,
+        HeadOfDepartment,
+        HeadMaster
+    }
+
+    #region Form class
+    public partial class AddEmpWin : Form
+    {
+        #region Variables area
+        List<IEmployee> employees;   // List of type employee interface to store all profiles
+        int ProfileIndex = -1;       // Record the last index of added profiles
+        #endregion 
+
+        public AddEmpWin()
+        {
+            InitializeComponent();
+        }
+
+        #region User defined methods
+        // Insert data form the list to the fields
+        private void GetProfile()
+        {
+            if (ProfileIndex > -1)
+            {
+                EmpIDTxt.Text = employees[ProfileIndex].Id.ToString();
+                EmpNameTxt.Text = employees[ProfileIndex].Name;
+                if (employees[ProfileIndex].Job == "Teacher")
+                  EmpJobCombo.SelectedItem = EmployeeType.Teacher;
+                if (employees[ProfileIndex].Job == "Head of department")
+                    EmpJobCombo.SelectedItem = EmployeeType.HeadOfDepartment;
+                if (employees[ProfileIndex].Job == "Head master")
+                    EmpJobCombo.SelectedItem = EmployeeType.HeadMaster;
+                EmpSalaryTxt.Text = employees[ProfileIndex].Salary.ToString();
+            }
+            // If the list is empty
+            else
+            {
+                EmpIDTxt.Clear();
+                EmpNameTxt.Clear();
+                EmpJobCombo.SelectedItem = EmployeeType.Teacher;
+                EmpSalaryTxt.Clear();
+            }
+
+        }
+        
+        // Insert the total number of employees and total salaries into the form
+        private void UpdateTotal()
+        {
+            NumOfEmpLbl.Text = employees.Count.ToString();
+
+            // Calculate the total salaries using lambda expression
+            TotalSalShowLbl.Text = employees.Sum(emps => emps.Salary).ToString();
+        }
+        #endregion
+
+        #region form events
+        // Form load event
+        private void AddEmpWin_Load(object sender, EventArgs e)
+        {
+            employees = new List<IEmployee>();
+
+            // Insert the employees enum as a data source for the employee type combobox
+            EmpJobCombo.DataSource = Enum.GetValues(typeof(EmployeeType));
+        }
+
+        // Add profile button click event
+        private void AddEmpBtn_Click(object sender, EventArgs e)
+        {
+            // Convert selected item from job combobox to job's type enum and store it in a variable
+            EmployeeType EmpTypeTemp = (EmployeeType)EmpJobCombo.SelectedItem;
+
+            int id = int.Parse(EmpIDTxt.Text);
+            decimal salary = decimal.Parse(EmpSalaryTxt.Text);
+
+            employees.Add(EmployeeFactory.GetEmployeeInstane(id, EmpNameTxt.Text, EmpTypeTemp, salary));
+
+            ProfileIndex = employees.Count - 1;
+
+            UpdateTotal();
+        }
+
+        // Clear profile button click event
+        private void ClearEmpBtn_Click(object sender, EventArgs e)
+        {
+            if (ProfileIndex > -1)
+            {
+                employees.RemoveAt(ProfileIndex);
+
+                // If the deleted profile was the last one, decrease the profile by one
+                if (ProfileIndex == employees.Count)
+                    ProfileIndex--;
+
+                GetProfile();
+                UpdateTotal();
+            }
+        }
+
+        // Id field key press event
+        private void EmpIDTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Force the field to accept numbers and backspace signals only
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == Convert.ToChar(Keys.Back))
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        // Employee salary key press event
+        private void EmpSalaryTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == Convert.ToChar(Keys.Back) || e.KeyChar == '.')
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        // First profile button click event
+        private void FirstProfileB_Click(object sender, EventArgs e)
+        {
+            if (ProfileIndex > 0)
+                ProfileIndex = 0; GetProfile();
+        }
+
+        // Previous profile button click event
+        private void PreviousProfileB_Click(object sender, EventArgs e)
+        {
+            if (ProfileIndex > 0)
+                --ProfileIndex; GetProfile();
+        }
+
+        // Next profile button click event
+        private void NextProfileB_Click(object sender, EventArgs e)
+        {
+            if (ProfileIndex < employees.Count - 1)
+                ++ProfileIndex; GetProfile();
+        }
+
+        // Last profile button click event
+        private void LastProfileB_Click(object sender, EventArgs e)
+        {
+            if (ProfileIndex < employees.Count - 1)
+                ProfileIndex = employees.Count - 1; GetProfile();
+        }
+        #endregion
+    }
+    #endregion
+
+    #region Employee classes
+    public class Teacher : EmployeeBase
+    {
+        public override decimal Salary => base.Salary + (base.Salary * 0.02m);
+    }
+
+    public class HeadOfDepartment : EmployeeBase
+    {
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.03m); }
+    }
+
+    public class HeadMaster : EmployeeBase
+    {
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); }
+    }
+    #endregion
+
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstane(int id, string name, EmployeeType employeeType, decimal salary)
+        {
+            IEmployee employee = null;
+
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = new Teacher { Id = id, Name = name, Job = "Teacher", Salary = salary };
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = new HeadOfDepartment { Id = id, Name = name, Job = "Head of department", Salary = salary };
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = new HeadMaster { Id = id, Name = name, Job = "Head master", Salary = salary };
+                    break;
+                default:
+                    break;
+            }
+            return employee;
+        }
+    }
+}
